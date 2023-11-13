a226b21dbbdb92bd4f38d4f316391a94d40e9146
elmurod <elmurodraimov764@gmail.com>
2023-11-13 15:01:57 +0500

    code

--- String10.py
+++ String10.py
@@ -1,10 +1,5 @@
 def main(s):
-    """
-    A string containing the letter "x" is given. Find the index of the letter "x" in this variable.
-    Args:
-        s: str
-    Returns:
-        str: answer
-    """
-    
-    return
+   
+    return s.index('x')
+print(main("Mobile tpye xiomi"))
+print(main("iphone x is a mobile"))
