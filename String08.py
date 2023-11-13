d3cb8a7526fee335305828b44afb9801b0d0d1ac
elmurod <elmurodraimov764@gmail.com>
2023-11-13 15:44:31 +0500

    code

--- String08.py
+++ String08.py
@@ -1,10 +1,5 @@
 def main(s):
-    """
-    A variable of type str is given. Make sure it only consists of uppercase letters.
-    Args:
-        s: str
-    Returns:
-        bool: answer
-    """
-    
-    return
+   
+    return s.isupper()
+print(main("CODESCHOOL"))
+print(main("Mobile"))
