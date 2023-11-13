6c80c9b612e2eb35ba275638ef6c7ec2cefe0147
elmurod <elmurodraimov764@gmail.com>
2023-11-13 15:38:01 +0500

    code

--- String07.py
+++ String07.py
@@ -1,10 +1,5 @@
 def main(s):
-    """
-    A variable of type str is given. Check that it consists of letters only.
-    Args:
-        s: str
-    Returns:
-        bool: answer
-    """
     
-    return
+    return s.isalpha()
+print(main("12345"))
+print(main("2022ABC"))
